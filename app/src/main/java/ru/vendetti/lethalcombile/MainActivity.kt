1b02384bf25d1573ba99e6c51823b69003f2ae37
Andy4Vendetta <cellinia.texas@yandex.com>
2024-04-22 22:20:18 +0500

    Added dev branch

--- app/src/main/java/ru/vendetti/lethalcombile/MainActivity.kt
+++ app/src/main/java/ru/vendetti/lethalcombile/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             LethalCombileTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Aboba")
                 }
             }
         }
