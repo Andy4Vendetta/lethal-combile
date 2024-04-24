6f6cfdb3f3064c0f08d8c158d180a6ebc577a348
Andy4Vendetta <cellinia.texas@yandex.com>
2024-04-24 12:08:43 +0500

    Work on login/register screen started.

--- app/src/main/java/ru/vendetti/lethalcombile/ui/theme/Color.kt
+++ app/src/main/java/ru/vendetti/lethalcombile/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LethalTerminalBack = Color(0xFF282D29)
+val LethalTerminalText = Color(0xFF299696)
+val LethalTerminalTextDark = Color(0xFF207070)
+val LethalTerminalBorder = Color(0xFF4D4D4D)
