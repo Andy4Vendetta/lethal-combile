25a38df1dfe25c53a31aeb6c4c4fde31ba05ba20
Andy4Vendetta <cellinia.texas@yandex.com>
2024-04-30 19:41:32 +0500

    Register/login system v1 developed

--- app/src/main/java/ru/vendetti/lethalcombile/MainActivity.kt
+++ app/src/main/java/ru/vendetti/lethalcombile/MainActivity.kt
@@ -3,6 +3,8 @@
 package ru.vendetti.lethalcombile
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
@@ -29,6 +31,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import ru.vendetti.lethalcombile.ui.theme.LethalCombileTheme
 import ru.vendetti.lethalcombile.ui.theme.LethalTerminalBack
 import ru.vendetti.lethalcombile.ui.theme.LethalTerminalBorder
@@ -38,97 +43,175 @@ import ru.vendetti.lethalcombile.ui.theme.LethalTerminalTextDark
 import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
+    private var auth: FirebaseAuth = Firebase.auth
+    private var currentUser = auth.currentUser
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //if (currentUser != null) {
+        //reload()
+        //} else {
         super.onCreate(savedInstanceState)
         setContent { LoginAppScreen() }
+        //}
     }
-}
-@Composable
-fun LoginAppScreen(){
-    var errorText by remember { mutableStateOf("")}
-    var text by remember { mutableStateOf("")}  // Состояние для хранения введённого текста
-    val keyboardController = LocalSoftwareKeyboardController.current
-    LaunchedEffect(Unit) {
-        keyboardController?.show()
-    }
-    LethalCombileTheme {
-        Surface(modifier = Modifier.fillMaxSize(), color = LethalTerminalBack) {
-            Box(
-                modifier = Modifier
-                    .border(30.dp, LethalTerminalBorder)
-                    .padding(30.dp)
-                    .fillMaxSize()
-            ) {
-                Column(
-                    modifier = Modifier.padding(20.dp)
+
+    @Composable
+    fun LoginAppScreen() {
+        var errorText by remember { mutableStateOf("") }
+        var text by remember { mutableStateOf("") }  // Состояние для хранения введённого текста
+        val keyboardController = LocalSoftwareKeyboardController.current
+        LaunchedEffect(Unit) {
+            keyboardController?.show()
+        }
+        LethalCombileTheme {
+            Surface(modifier = Modifier.fillMaxSize(), color = LethalTerminalBack) {
+                Box(
+                    modifier = Modifier
+                        .border(30.dp, LethalTerminalBorder)
+                        .padding(30.dp)
+                        .fillMaxSize()
                 ) {
-                    Text("[WELCOME]\n", color = LethalTerminalText, fontSize = 25.sp)
-                    HorizontalDivider(color = LethalTerminalText)
-                    Text("\nRegister or login before starting the game. You can do it by typing:", color = LethalTerminalText, fontSize = 20.sp)
-                    Text("register <emal> <password> <password>\n", color = LethalTerminalTextDark, fontSize = 16.sp)
-                    Text("login <email> <password>\n", color = LethalTerminalTextDark, fontSize = 16.sp)
-                    HorizontalDivider(color = LethalTerminalText)
-                    Text("\nAll available commands:", color = LethalTerminalText, fontSize = 20.sp)
-                    Text("register\nlogin\nexit", color = LethalTerminalTextDark, fontSize = 16.sp)
-                    Text("\nYou can type your command right below:", color = LethalTerminalText, fontSize = 20.sp)
-                    Text(errorText, color = LethalTerminalRed, fontSize = 20.sp)
-                    BasicTextField(
-                        value = text,
-                        onValueChange = { text = it },
-                        singleLine = true,
-                        textStyle = TextStyle(color = LethalTerminalText, fontSize = 20.sp),
-                        cursorBrush = SolidColor(LethalTerminalText),
-                        modifier = Modifier
-                            .padding(top = 30.dp)
-                            .fillMaxSize(),
-                        keyboardOptions = KeyboardOptions.Default.copy(
-                            imeAction = ImeAction.Done
-                        ),
-                        keyboardActions = KeyboardActions(onDone = {
-                            executeCommand(text) { newErrorText ->
-                                errorText = newErrorText
-                            }
-                        })
-                    )
+                    Column(
+                        modifier = Modifier.padding(20.dp)
+                    ) {
+                        Text("[WELCOME]\n", color = LethalTerminalText, fontSize = 25.sp)
+                        HorizontalDivider(color = LethalTerminalText)
+                        Text(
+                            "\nRegister or login before starting the game. You can do it by typing:",
+                            color = LethalTerminalText,
+                            fontSize = 20.sp
+                        )
+                        Text(
+                            "register <emal> <password> <password>\n",
+                            color = LethalTerminalTextDark,
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            "login <email> <password>\n",
+                            color = LethalTerminalTextDark,
+                            fontSize = 16.sp
+                        )
+                        HorizontalDivider(color = LethalTerminalText)
+                        Text(
+                            "\nAll available commands:",
+                            color = LethalTerminalText,
+                            fontSize = 20.sp
+                        )
+                        Text(
+                            "register\nlogin\nlogout\nexit",
+                            color = LethalTerminalTextDark,
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            "\nYou can type your command right below:",
+                            color = LethalTerminalText,
+                            fontSize = 20.sp
+                        )
+                        Text(errorText, color = LethalTerminalRed, fontSize = 20.sp)
+                        BasicTextField(
+                            value = text,
+                            onValueChange = { text = it },
+                            singleLine = true,
+                            textStyle = TextStyle(color = LethalTerminalText, fontSize = 20.sp),
+                            cursorBrush = SolidColor(LethalTerminalText),
+                            modifier = Modifier
+                                .padding(top = 30.dp)
+                                .fillMaxSize(),
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                imeAction = ImeAction.Done
+                            ),
+                            keyboardActions = KeyboardActions(onDone = {
+                                executeCommand(text) { newErrorText ->
+                                    errorText = newErrorText
+                                }
+                            })
+                        )
+                    }
                 }
             }
         }
     }
-}
-fun executeCommand(
-    text: String,
-    setErrorText: (String) -> Unit
-) {
-    when (text.trim().split(" ")[0]) {
-        "exit" -> exitProcess(-1)
-        "login" -> checkLogin(text, setErrorText)
-        "register" -> checkRegister(text, setErrorText)
-        else -> {
-            setErrorText("This command doesn't exist!")
+
+    private fun executeCommand(
+        text: String, setErrorText: (String) -> Unit
+    ) {
+        when (text.trim().split(" ")[0]) {
+            "exit" -> exitProcess(-1)
+            "login" -> checkLogin(text, setErrorText)
+            "register" -> checkRegister(text, setErrorText)
+            "logout" -> logout(setErrorText)
+            else -> {
+                setErrorText("This command doesn't exist!")
+            }
         }
     }
-}
-fun checkLogin(text: String, setErrorText: (String) -> Unit) {
-    val parts = text.split(" ")
-    if(parts.size == 2){
-        if(parts[1] == "aboba"){
-            setErrorText("SUS")
+
+    private fun checkLogin(text: String, setErrorText: (String) -> Unit) {
+        val parts = text.split(" ")
+        if (parts.size == 3) {
+            if (isValidEmail(parts[1]) && parts[2].length > 5) {
+                setErrorText("Logging in...")
+                login(parts[1], parts[2], setErrorText)
+            } else {
+                setErrorText("Incorrect email/passwd (len must be > 5 chars)")
+            }
         } else {
-            setErrorText("Incorrect password")
+            setErrorText("Wrong syntax")
         }
-    } else {
-        setErrorText("Wrong syntax")
     }
-}
-fun checkRegister(text: String, setErrorText: (String) -> Unit){
-    val parts = text.split(" ")
-    if(parts.size == 3){
-        if(parts[1] == "hmmm" && parts[1] == parts[2]){
-            setErrorText("ladno")
+
+    private fun checkRegister(text: String, setErrorText: (String) -> Unit) {
+        val parts = text.split(" ")
+        if (parts.size == 4) {
+            if (isValidEmail(parts[1]) && parts[2].length > 5 && parts[2] == parts[3]) {
+                setErrorText("Registering...")
+                register(parts[1], parts[2], setErrorText)
+            } else {
+                setErrorText("Incorrect email/passwd (len must be > 5 chars)")
+            }
         } else {
-            setErrorText("Incorrect password")
+            setErrorText("Wrong syntax")
         }
-    } else {
-        setErrorText("Wrong syntax")
+    }
+
+    private fun isValidEmail(target: CharSequence?): Boolean {
+        return !TextUtils.isEmpty(target) && target?.let {
+            Patterns.EMAIL_ADDRESS.matcher(it).matches()
+        } == true
+    }
+
+    private fun register(email: String, password: String, setErrorText: (String) -> Unit) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    currentUser = auth.currentUser
+                    setErrorText("Successful")
+                } else {
+                    setErrorText("Register error")
+                }
+            }
+    }
+
+    private fun login(email: String, password: String, setErrorText: (String) -> Unit) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    currentUser = auth.currentUser
+                    setErrorText("Successful")
+                    //reload()
+                    //updateUI(user)
+                } else {
+                    setErrorText("Login error")
+                }
+            }
+    }
+
+    private fun logout(setErrorText: (String) -> Unit) {
+        if (currentUser == null) {
+            setErrorText("You're not logged in")
+        } else {
+            Firebase.auth.signOut()
+            currentUser = auth.currentUser
+            setErrorText("Logged out")
+        }
     }
 }
